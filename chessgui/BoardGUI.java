31af56ddd61a8968faf0a3bfd3f1bedfedec26ad
JollySeason <154002969+JollySeason@users.noreply.github.com>
2024-04-25 10:01:45 -0400

    Add files via upload

--- chessgui/BoardGUI.java
+++ chessgui/BoardGUI.java
@@ -6,15 +6,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 /**
  * tan color: new Color(250, 230, 175). Green color: new Color(145, 210, 100).
  */
 public class BoardGUI extends JPanel {
 	private static final long serialVersionUID = 1L;
-
-	private final Image PAWN_IMAGE = new ImageIcon(getClass().getResource("/images/used-rook-white.png")).getImage();
-
 	private JPanel boardPanel;
 
 	public BoardGUI() {
@@ -22,58 +20,67 @@ public class BoardGUI extends JPanel {
 	}
 
 	public BoardGUI(Board board) {
-		setBackground(Color.DARK_GRAY);
+		setBackground(Color.WHITE);
 
 		boardPanel = new JPanel();
 		boardPanel.setLayout(new GridLayout(9, 9));
 		Piece[][] boardArr = board.getBoard();
 
-		// add coordinates outside of the board (a-h)
+		// add letter coordinates outside of the board (a-h)
 		for (int i = 'a'; i < 'i'; i++) {
 			JLabel temp = new JLabel("" + (char) i);
 			temp.setFont(new Font("ARIAL", Font.PLAIN, 20));
+			temp.setForeground(Color.WHITE);
 			temp.setHorizontalAlignment(SwingConstants.CENTER);
 
 			boardPanel.add(temp);
 		}
 		boardPanel.add(new JLabel());
 
-		// add each chess square and add more coordinates (1-8)
-		System.out.println(boardArr.length);
+		// add each chess square
 		for (int i = 1; i <= 8; i++) {
 			for (int j = 1; j <= 8; j++) {
-				boardPanel.add(createSquare("" + (char) (j+64) + (9-i), Runner.getScaledImage(
-						boardArr[i - 1][j - 1] == null ? null : boardArr[i - 1][j - 1].getPieceSprite(), 80, 80),
-						((((j) % 2) + (i % 2)) % 2 == 1) ? new Color(65, 130, 185) : new Color(230, 230, 230)));
+				JPanel tempPanel = createSquare("" + (char) (j + 64) + (9 - i),
+						((((j) % 2) + (i % 2)) % 2 == 1) ? new Color(65, 130, 185) : new Color(230, 230, 230));
+				if (boardArr[i - 1][j - 1] != null)
+					tempPanel.add(boardArr[i - 1][j - 1].getPieceSprite());
+				tempPanel.setBorder(new EmptyBorder(-5, 0, 0, 0));
+				boardPanel.add(tempPanel);
 			}
 
-			JLabel temp = new JLabel("" + (9-i));
-			temp.setFont(new Font("ARIAL", Font.PLAIN, 20));
-			temp.setForeground(Color.WHITE);
-			temp.setHorizontalAlignment(SwingConstants.CENTER);
-			boardPanel.add(temp);
+			// adds the number coordinates on the side of the board (1-8)
+			JLabel coordNumberLabel = new JLabel("" + (9 - i));
+			coordNumberLabel.setFont(new Font("ARIAL", Font.PLAIN, 20));
+			coordNumberLabel.setForeground(Color.WHITE);
+			coordNumberLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			boardPanel.add(coordNumberLabel);
 		}
-		
-		boardPanel.setBackground(this.getBackground());
+
+		boardPanel.setBackground(Color.RED);
 
 		add(boardPanel);
 	}
 
-	private JButton createSquare(String pos, Image image, Color color) {
-		JButton butt = new JButton(new ImageIcon(Runner.getScaledImage(image, 80, 80)));
-		butt.setFocusable(false);
-		butt.setBorderPainted(false);
-		butt.setActionCommand(pos);
+	private JPanel createSquare(String pos, Color color) {
+		GridSpace temp = new GridSpace(pos);
+		temp.setFocusable(false);
+		temp.setPreferredSize(new Dimension(80, 80));
+		temp.setBackground(color);
 
-		butt.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println(e.getActionCommand());
-			}
-		});
-
-		butt.setPreferredSize(new Dimension(80, 80));
-		butt.setBackground(color);
-		return butt;
+		return temp;
 	}
 
+//	exists so that the square can hold the value of a position
+	private class GridSpace extends JPanel {
+		private static final long serialVersionUID = 1L;
+		private String position;
+
+		public GridSpace(String pos) {
+			position = pos;
+		}
+
+		public String getPosition() {
+			return position;
+		}
+	}
 }
--- chessgui/TitlePane.java
+++ chessgui/TitlePane.java
@@ -12,12 +12,14 @@ public class TitlePane extends JPanel {
 	private JPanel buttonContents;
 
 	private JButton startButton;
+	private JButton settingsButton;
 	
 	private JLabel titleLabel;
 
 	public TitlePane() {
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		
+//		add titleContents components
 		titleContents = new JPanel();
 		titleContents.setMaximumSize(new Dimension(2000, 200));
 		
@@ -25,24 +27,42 @@ public class TitlePane extends JPanel {
 		titleLabel.setFont(new Font("Leelawadee", Font.PLAIN, 150));
 
 		
-		
+//		add button components
 		buttonContents = new JPanel();
+		buttonContents.setLayout(new BoxLayout(buttonContents, BoxLayout.PAGE_AXIS));
 		
-		startButton = new JButton("start");
-		startButton.setAlignmentX(CENTER_ALIGNMENT);
+		startButton = getFormattedButton("start");
 		startButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				Runner.setScreen(Runner.board);
 			}
 		});
 		
+		settingsButton = getFormattedButton("settings");
+		settingsButton.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+//				Runner.setScreen(Runner.settings);
+			}});
 		
 		titleContents.add(titleLabel);
 		
 		buttonContents.add(startButton);
-		
+		buttonContents.add(settingsButton);
 		
 		add(titleContents);
 		add(buttonContents);
+		
+	}
+	
+	private JButton getFormattedButton(String name) {
+		JButton temp = new JButton(name);
+		temp.setSize(new Dimension(250, 50));
+		
+		temp.setFocusable(false);
+		temp.setForeground(Color.WHITE);
+		temp.setBackground(Color.BLACK);
+		temp.setAlignmentX(CENTER_ALIGNMENT);
+		
+		return temp;
 	}
 }
