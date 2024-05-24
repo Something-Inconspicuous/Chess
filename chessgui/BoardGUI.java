cf4e5487e4680a8e74c257137ee1345916505910
JollySeason <154002969+JollySeason@users.noreply.github.com>
2024-05-24 10:32:08 -0400

    Add files via upload

--- chessgui/BoardGUI.java
+++ chessgui/BoardGUI.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.util.HashMap;
 
 /**
@@ -14,35 +13,37 @@ import java.util.HashMap;
  */
 public class BoardGUI extends JPanel {
 	private static final long serialVersionUID = 1L;
+	private JPanel topUserPanel; // holds stats and pieces captured by the user on the top
+	private JPanel bottomUserPanel; // holds stats and pieces captured by the user on the bottom
+	private JPanel sidePanel; // holds move history, eval bar, etc
 	private JPanel boardPanel;
 	private Board board;
 	private HashMap<String, JPanel> posToSquareMap;
-	private JButton removeAllDots; 
-	
+	private JButton removeAllDots;
 
 	public BoardGUI() {
+		setBackground(Color.DARK_GRAY);
 		board = Runner.board;
-		setBackground(Color.WHITE);
 
 		boardPanel = new JPanel();
+		boardPanel.setBackground(this.getBackground());
 		boardPanel.setLayout(new GridLayout(9, 9));
-	
-		
+
 		removeAllDots = new JButton();
-		
+
 		posToSquareMap = new HashMap<String, JPanel>();
 		Piece[][] boardArr = board.getBoard();
 
 		// add letter coordinates outside of the board (a-h)
 		for (int i = 'a'; i < 'i'; i++) {
-			JLabel temp = new JLabel("" + (char) i);
-			temp.setFont(new Font("ARIAL", Font.PLAIN, 20));
-			temp.setForeground(Color.WHITE);
-			temp.setHorizontalAlignment(SwingConstants.CENTER);
+			JLabel coordLetterLabel = new JLabel("" + (char) i);
+			coordLetterLabel.setFont(new Font("ARIAL", Font.PLAIN, 20));
+			coordLetterLabel.setForeground(Color.WHITE);
+			coordLetterLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-			boardPanel.add(temp);
+			boardPanel.add(coordLetterLabel);
 		}
-		
+
 		boardPanel.add(new JLabel());
 
 		// add each chess square
@@ -52,10 +53,10 @@ public class BoardGUI extends JPanel {
 						((((j) % 2) + (i % 2)) % 2 == 1) ? new Color(65, 130, 185) : new Color(230, 230, 230));
 				if (boardArr[i - 1][j - 1] != null)
 					tempPanel.add(boardArr[i - 1][j - 1].getPieceSprite());
-				
+
 				boardPanel.add(tempPanel);
-				
-				removeAllDots.addActionListener((GridSpace)tempPanel);
+
+				removeAllDots.addActionListener((GridSpace) tempPanel);
 			}
 
 			// adds the number coordinates on the side of the board (1-8)
@@ -66,42 +67,40 @@ public class BoardGUI extends JPanel {
 			boardPanel.add(coordNumberLabel);
 		}
 
-		boardPanel.setBackground(Color.GRAY);
+		boardPanel.setBackground(this.getBackground());
 
 		add(boardPanel);
 	}
-	
-	
+
 	public JPanel getBoardPanel() {
 		return boardPanel;
 	}
-	
+
 	public Board getBoard() {
 		return board;
 	}
-	
+
 	public void clearBoard() {
-		 removeAllDots.doClick();
+		removeAllDots.doClick();
 	}
 
 	private JPanel createSquare(String pos, Color color) {
 		GridSpace temp = new GridSpace(pos, color);
 		temp.setLayout(new OverlayLayout(temp));
-		
+
 		temp.setFocusable(false);
 		temp.setPreferredSize(new Dimension(80, 80));
 		temp.setBackground(color);
-		System.out.println(pos);
 		posToSquareMap.put(pos, temp);
 		return temp;
 	}
-	
+
 	public HashMap<String, JPanel> getPositionMap() {
 		return posToSquareMap;
 	}
 
 //	exists so that the square can hold the value of a position
-	private class GridSpace extends JPanel implements ActionListener{
+	private class GridSpace extends JPanel implements ActionListener {
 		private static final long serialVersionUID = 1L;
 		private String position;
 		private Color color;
@@ -117,10 +116,10 @@ public class BoardGUI extends JPanel {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
-			if(getComponentCount() > 0) {
-				ImageIcon tempIcon = (ImageIcon)((JButton)getComponent(0)).getIcon();
-				if(tempIcon == Runner.moveCircle || tempIcon == Runner.captureCircle) {
+
+			if (getComponentCount() > 0) {
+				ImageIcon tempIcon = (ImageIcon) ((JButton) getComponent(0)).getIcon();
+				if (tempIcon == Runner.moveCircle || tempIcon == Runner.captureCircle) {
 					remove(0);
 					repaint();
 				}
